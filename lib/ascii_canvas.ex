c0d974729ea6b90e6a2351595c6755cd1169aaa4
Getty Orawo <gettyorawo@gmail.com>
2021-03-27 23:15:32 +0300

    collect outline coordinates if there's an outline

--- lib/ascii_canvas.ex
+++ lib/ascii_canvas.ex
@@ -6,29 +6,66 @@ defmodule AsciiCanvas do
   defstruct [:origin, :start, :width, :height, :outline, :fill]
 
   @doc """
-  navigator reducer function moves right from current cell
+  constructor function: create struct
   """
+  def new([x,y], w, h, outline, fill) do
+
+    strct = %__MODULE__{ origin: {x,y}, width: w, height: h, outline: outline, fill: fill }
+    draw_outline([], strct, 0, {x,y})
+
+  end
+
+  @doc """
+  fetches all the coordinates for an outline, if one is provided
+  """
+
+  def draw_outline(coordinates, strct, acc, org) when acc < strct.width-1 do
+      curr = org |> right
+      coordinates
+      |> List.insert_at(0, curr)
+      |> draw_outline(strct, acc+1, curr)
+  end
+
+  def draw_outline(coordinates, strct, acc, org) when acc < (strct.width + strct.height)-2 do
+      curr = org |> down
+      coordinates
+      |> List.insert_at(0, curr)
+      |> draw_outline(strct, acc+1, curr)
+  end
+
+  def draw_outline(coordinates, strct, acc, org) when acc < (strct.width + strct.height + strct.width)-3 do
+      curr = org |> left
+      coordinates
+      |> List.insert_at(0, curr)
+      |> draw_outline(strct, acc+1, curr)
+  end
+
+  def draw_outline(coordinates, strct, acc, org) when acc < (strct.width + strct.height + strct.width + strct.height)-4 do
+      curr = org |> up
+      coordinates
+      |> List.insert_at(0, curr)
+      |> draw_outline(strct, acc+1, curr)
+  end
+
+  def draw_outline(coordinates, strct, _acc, _org) do
+      c = coordinates
+      |> Enum.sort()
+
+      %{character: strct.outline, coordinates: c}
+  end
+
   defp right({x,y}) do
     {x+1, y}
   end
 
-  @doc """
-  navigator reducer function moves left from current cell
-  """
   defp left({x,y}) do
     {x-1, y}
   end
 
-  @doc """
-  navigator reducer function moves down from current cell
-  """
   defp down({x,y}) do
     {x, y+1}
   end
 
-  @doc """
-  navigator reducer function moves up from current cell
-  """
   defp up({x,y}) do
     {x, y-1}
   end
