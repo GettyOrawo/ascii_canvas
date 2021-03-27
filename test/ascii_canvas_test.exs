afb5674b87e00a3ac950b999f1006dbeb61fe834
Getty Orawo <gettyorawo@gmail.com>
2021-03-27 23:07:19 +0300

    add test fixture one for outline

--- test/ascii_canvas_test.exs
+++ test/ascii_canvas_test.exs
@@ -2,7 +2,22 @@ defmodule AsciiCanvasTest do
   use ExUnit.Case
   doctest AsciiCanvas
 
-  test "greets the world" do
-    assert AsciiCanvas.hello() == :world
+  test "gives outline co-ordinates" do
+    assert AsciiCanvas.new([3,2],5,3,"@","x") == %{character: "@", 
+    												coordinates: [
+													  {3, 2},
+													  {3, 3},
+													  {3, 4},
+													  {4, 2},
+													  {4, 4},
+													  {5, 2},
+													  {5, 4},
+													  {6, 2},
+													  {6, 4},
+													  {7, 2},
+													  {7, 3},
+													  {7, 4}
+													]
+												 }
   end
 end
