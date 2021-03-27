4e5e3e8c619ee7c4b708e2a8211727f60e269c05
Getty Orawo <gettyorawo@gmail.com>
2021-03-27 23:10:53 +0300

    introduce navigator functions for left,right, up and down

--- lib/ascii_canvas.ex
+++ lib/ascii_canvas.ex
@@ -3,10 +3,33 @@ defmodule AsciiCanvas do
   Creates a canvas board and fills it with the provided outline and/or fill characters
   """
 
-  @doc """
-  define constructor function
-  """
+  defstruct [:origin, :start, :width, :height, :outline, :fill]
 
-  def new do
+  @doc """
+  navigator reducer function moves right from current cell
+  """
+  defp right({x,y}) do
+    {x+1, y}
+  end
+
+  @doc """
+  navigator reducer function moves left from current cell
+  """
+  defp left({x,y}) do
+    {x-1, y}
+  end
+
+  @doc """
+  navigator reducer function moves down from current cell
+  """
+  defp down({x,y}) do
+    {x, y+1}
+  end
+
+  @doc """
+  navigator reducer function moves up from current cell
+  """
+  defp up({x,y}) do
+    {x, y-1}
   end
 end
