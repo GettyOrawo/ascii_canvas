f01fb28e463be03027dbaafa0731b14cab4a85ad
Getty Orawo <gettyorawo@gmail.com>
2021-03-30 14:31:01 +0300

    cases of nil outline or fill for rectangles

--- lib/ascii_canvas.ex
+++ lib/ascii_canvas.ex
@@ -6,67 +6,99 @@ defmodule AsciiCanvas do
   defstruct [:origin, :start, :width, :height, :outline, :fill]
 
   @doc """
-  constructor function: create struct
+  constructor: creates initial state and draws a rectangle
   """
+  
   def new([x,y], w, h, outline, fill) do
-
-    strct = %__MODULE__{ origin: {x,y}, width: w, height: h, outline: outline, fill: fill }
-    draw_outline([], strct, 0, {x,y})
-
+    %__MODULE__{ origin: {x,y}, width: w, height: h, outline: outline, fill: fill }
+    |> draw()
   end
 
   @doc """
-  fetches all the coordinates for an outline, if one is provided
+  fetches all the outline coordinates for a given rectangle, if outline is specified
   """
 
+  def draw(strct) when strct.outline == nil do
+    strct
+    |> draw_fill()
+    |> Map.put(:outline, nil)
+  end
+
+  def draw(strct) when strct.fill == nil do
+    []
+    |> draw_outline(strct, 0, strct.origin)
+    |> Map.put(:fill, nil)
+  end
+
+  def draw(strct) do
+    draw_outline_and_fill(strct)
+  end
+  
+
+  @doc """
+  fetches all the outline and fill coordinates for a given rectangle, if outline is specified
+  """
   def draw_outline(coordinates, strct, acc, org) when acc < strct.width-1 do
-      curr = org |> right
       coordinates
-      |> List.insert_at(0, curr)
-      |> draw_outline(strct, acc+1, curr)
+      |> List.insert_at(0, org)
+      |> draw_outline(strct, acc+1, move_right(org))
   end
 
   def draw_outline(coordinates, strct, acc, org) when acc < (strct.width + strct.height)-2 do
-      curr = org |> down
       coordinates
-      |> List.insert_at(0, curr)
-      |> draw_outline(strct, acc+1, curr)
+      |> List.insert_at(0, org)
+      |> draw_outline(strct, acc+1, move_down(org))
   end
 
   def draw_outline(coordinates, strct, acc, org) when acc < (strct.width + strct.height + strct.width)-3 do
-      curr = org |> left
       coordinates
-      |> List.insert_at(0, curr)
-      |> draw_outline(strct, acc+1, curr)
+      |> List.insert_at(0, org)
+      |> draw_outline(strct, acc+1, move_left(org))
   end
 
   def draw_outline(coordinates, strct, acc, org) when acc < (strct.width + strct.height + strct.width + strct.height)-4 do
-      curr = org |> up
       coordinates
-      |> List.insert_at(0, curr)
-      |> draw_outline(strct, acc+1, curr)
+      |> List.insert_at(0, org)
+      |> draw_outline(strct, acc+1, move_up(org))
   end
 
   def draw_outline(coordinates, strct, _acc, _org) do
       c = coordinates
       |> Enum.sort()
-
-      %{character: strct.outline, coordinates: c}
+      %{outline: %{character: strct.outline, coordinates: c}}
   end
 
-  defp right({x,y}) do
+  def draw_outline_and_fill(strct) do
+    draw_outline([], strct, 0, strct.origin)
+    |> Map.merge(draw_fill(strct))
+  end
+
+  @doc """
+  fetches all the fill coordinates for a given rectangle, if outline is 'none'
+  """
+  def draw_fill(strct) do
+    {x,y} = strct.origin
+    x_coordinates = Enum.to_list(x+1..x+(strct.width-2))
+    y_coordinates = Enum.to_list(y+1..y+(strct.height-2))
+    c = for n <- x_coordinates, t <- y_coordinates do
+      {n,t}
+    end
+    %{fill: %{character: strct.fill, coordinates: c}}
+  end
+
+  defp move_right({x,y}) do
     {x+1, y}
   end
 
-  defp left({x,y}) do
+  defp move_left({x,y}) do
     {x-1, y}
   end
 
-  defp down({x,y}) do
+  defp move_down({x,y}) do
     {x, y+1}
   end
 
-  defp up({x,y}) do
+  defp move_up({x,y}) do
     {x, y-1}
   end
 end
